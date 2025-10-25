3cf8ac99edf85f22d65c15c09093d76b5aeea10f
Hariompal16 <ap9308719@gmail.com>
2025-10-25 20:12:44 +0530

    Fix import path for registerUser in Signup.jsx

--- leetcode_frontend/src/pages/Signup.jsx
+++ leetcode_frontend/src/pages/Signup.jsx
@@ -4,7 +4,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, NavLink } from 'react-router';
-import { registerUser } from '../authslice';
+import { registerUser } from '../authSlice';
 import { Mail, Lock, User as UserIcon, Eye, EyeOff, BookOpen, ArrowRight } from 'lucide-react';
 
 const signupSchema = z.object({
